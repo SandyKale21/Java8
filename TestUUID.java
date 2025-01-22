4dbae3932a41b71010e7d38ca5662dda60b7e8fb
SandyKale21 <sandeshkale@gmail.com>
2025-01-23 01:10:31 +0530

    Add files via upload

---
+++ TestUUID.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import java.util.Random;
+import java.util.UUID;
+
+public class TestUUID {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	
+
+        Random rand = new Random();
+		System.out.println();
+		
+	Long value = Math.abs(rand.nextLong());
+		
+		
+		
+		System.out.println(value);
+
+
+
+	}
+
+}
